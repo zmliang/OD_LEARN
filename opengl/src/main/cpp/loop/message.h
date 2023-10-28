de0699caf3e4781fba22b05fa4e6e4a6c9d94d55
“zmliang” <605593536@qq.com>
2023-10-28 18:33:56 +0400

    自定义message类，尝试实现消息自带callback功能

--- opengl/src/main/cpp/loop/message.h
+++ opengl/src/main/cpp/loop/message.h
@@ -7,6 +7,15 @@
 
 #include <functional>
 
+typename std::function<int()> INT_VOID_CALLBACK;
+
+typename std::function<int(int)> INT_INT_CALLBACK;
+
+typename std::function<void(int)> VOID_INT_CALLBACK;
+
+typename std::function<void()> VOID_VOID_CALLBACK;
+
+
 
 
 #endif //OD_LEARN_MESSAGE_H
