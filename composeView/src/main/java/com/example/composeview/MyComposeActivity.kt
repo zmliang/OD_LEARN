3d3a9875a23233520dda7da21c6a72788bf1bff8
“zmliang” <605593536@qq.com>
2023-11-05 17:11:49 +0400

    compose源码流程分析

--- composeView/src/main/java/com/example/composeview/MyComposeActivity.kt
+++ composeView/src/main/java/com/example/composeview/MyComposeActivity.kt
@@ -5,11 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -30,6 +27,31 @@ class MyComposeActivity : ComponentActivity() {
             Column {
                 messageBox()
                 HelloContent()
+                //第一个 padding(8.dp) 跟 margin 效果相同
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                        .background(Color.Cyan, RoundedCornerShape(12.dp))
+                        .padding(6.dp)
+                ) {
+                    Box(
+                        Modifier
+                            .background(Color.Red)
+                            .size(100.dp, 80.dp)
+                    )
+                    Spacer(
+                        Modifier
+                            .width(20.dp)
+                    )
+                    Box(
+                        Modifier
+                            .background(Color.Magenta)
+                            .height(60.dp)
+                            .weight(1f)
+                    )
+                }
+
             }
         }
     }
@@ -39,12 +61,16 @@ class MyComposeActivity : ComponentActivity() {
 @Composable
 fun HelloContent() {
     var clicked by remember { mutableStateOf(false) }
-    val alpha :Float by animateFloatAsState(targetValue = (if (clicked) 0.5f else 1.0f), label = "_alpha")
+    val alpha: Float by animateFloatAsState(
+        targetValue = (if (clicked) 0.5f else 1.0f),
+        label = "_alpha"
+    )
 
-    Column(modifier = Modifier
-        .padding(16.dp)
-        .graphicsLayer(alpha = alpha)
-        .background(color = Color.Blue)
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .graphicsLayer(alpha = alpha)
+            .background(color = Color.Blue)
     ) {
         var name by remember { mutableStateOf("") }
         Text(
@@ -61,9 +87,8 @@ fun HelloContent() {
 }
 
 
-
 @Composable
-fun messageBox(){
+fun messageBox() {
     Column {
         Text(text = "我是第一行")
         Text(text = "我是第二行")
