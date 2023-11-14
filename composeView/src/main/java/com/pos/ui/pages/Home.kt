768fd157edfddf1e068a5313a292fe0c274891e1
zmliang <605593536@qq.com>
2023-11-14 21:14:28 +0400

    首页下拉刷新，不ok的效果

--- composeView/src/main/java/com/pos/ui/pages/Home.kt
+++ composeView/src/main/java/com/pos/ui/pages/Home.kt
@@ -2,7 +2,11 @@ package com.pos.ui.pages
 
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.OverscrollEffect
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,7 +17,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -43,57 +50,85 @@ fun Home() {
         Token("BTC", "ICON", "CONTRACT", "DEXCONTRACT", null, null),
     )
 
-    LazyColumn(modifier = Modifier.fillMaxSize()){
 
-        item{
-            InfiniteCarousel()
-        }
-        item{
-            Resident()
-        }
 
-        stickyHeader {
-            Box(modifier = Modifier.height(50.dp).fillMaxWidth().background(Color.Green)){
-                Text(text = "这是吸顶的title")
+
+
+
+
+    PullRefresh(
+        onRefresh = {
+            delay(2000)
+        },
+        loadingIndicator = {
+            Box(modifier = Modifier.padding(10.dp)) {
+                CircularProgressIndicator(Modifier.size(200.dp))
             }
         }
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
 
-        allTokens(tokens)
+            item {
+                InfiniteCarousel()
+            }
+            item {
+                Resident()
+            }
 
+            stickyHeader {
+                Box(modifier = Modifier
+                    .height(50.dp)
+                    .fillMaxWidth()
+                    .background(Color.Green)) {
+                    Text(text = "这是吸顶的title")
+                }
+            }
+
+            allTokens(tokens)
+
+        }
     }
-
-//
-//    PullRefresh(
-//        onRefresh = {
-//            delay(2000)
-//        },
-//        loadingIndicator = {
-//            Box(modifier = Modifier.padding(10.dp)) {
-//                CircularProgressIndicator(Modifier.size(200.dp))
-//            }
-//        }
-//    ) {
-//
-//        Column(modifier = Modifier.pointerInput(Unit) {
-//
-//        }) {
-//
-//            InfiniteCarousel()
-//
-//            Resident()
-//
-//            TokenList(tokens = tokens)
-//        }
-//
-//    }
 
 }
 
-fun LazyListScope.allTokens(tokens:List<Token>){
 
-    items(tokens){
-        Box(modifier = Modifier.height(200.dp)){
-            Text(text = it.symbol+"/USDT")
+@Composable
+fun _test(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+            .scrollable(
+                state = rememberScrollableState(consumeScrollDelta = object : (Float) -> Float {
+                    override fun invoke(p1: Float): Float {
+                        return 100f
+                    }
+                }),
+                orientation = Orientation.Vertical,
+                enabled = true,
+
+                )
+    ) {
+        // Your scrollable content
+        Surface(
+            color = MaterialTheme.colorScheme.error,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "Sample Content",
+                modifier = Modifier.padding(16.dp),
+            )
+        }
+    }
+}
+fun LazyListScope.allTokens(tokens: List<Token>) {
+
+    items(tokens) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Text(text = it.symbol + "/USDT")
         }
     }
 }
