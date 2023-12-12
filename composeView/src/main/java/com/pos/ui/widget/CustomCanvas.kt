3d0fca6e1377a4dec90d0e0a28c91cf5c5481655
“zmliang” <605593536@qq.com>
2023-12-13 00:13:40 +0800

    上传大文件

--- composeView/src/main/java/com/pos/ui/widget/CustomCanvas.kt
+++ composeView/src/main/java/com/pos/ui/widget/CustomCanvas.kt
@@ -2,26 +2,21 @@ package com.pos.ui.widget
 
 import android.util.Log
 import android.view.ScaleGestureDetector
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.animate
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
@@ -30,61 +25,142 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextMeasurer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.times
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import androidx.compose.ui.unit.sp
 import java.lang.Float.max
 import java.lang.Float.min
+import kotlin.math.absoluteValue
 import kotlin.random.Random
 
 
 @Composable
 fun _canvas(){
 
-    KLineChartView(_sampleData, modifier = Modifier.fillMaxSize())
+    KLineChartView(_sampleData, modifier = Modifier
+        .fillMaxWidth()
+        .height(300.dp))
 
 }
 
 val _random = Random(200)
 var _sampleData = listOf(
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
-    KLineData(_random.nextFloat(), _random.nextFloat(), _random.nextFloat(), _random.nextFloat()),
+    KLineData(1L, 100f, 120f, 80f, 110f),
+    KLineData(2L, 110f, 130f, 90f, 120f),
+    KLineData(3L, 120f, 140f, 100f, 130f),
+    KLineData(4L, 130f, 150f, 110f, 140f),
+    KLineData(5L, 140f, 160f, 120f, 150f),
+    KLineData(5L, 140f, 160f, 120f, 120f),
+    KLineData(5L, 120f, 120f, 160f, 110f),
+
+    KLineData(1L, 100f, 120f, 80f, 110f),
+    KLineData(2L, 110f, 130f, 90f, 120f),
+    KLineData(3L, 120f, 140f, 100f, 130f),
+    KLineData(4L, 130f, 150f, 110f, 140f),
+    KLineData(5L, 140f, 160f, 120f, 150f),
+    KLineData(5L, 140f, 160f, 120f, 120f),
+    KLineData(5L, 120f, 120f, 160f, 110f),
+
+    KLineData(1L, 100f, 120f, 80f, 110f),
+    KLineData(2L, 110f, 130f, 90f, 120f),
+    KLineData(3L, 120f, 140f, 100f, 130f),
+    KLineData(4L, 130f, 150f, 110f, 140f),
+    KLineData(5L, 140f, 160f, 120f, 150f),
+    KLineData(5L, 140f, 160f, 120f, 120f),
+    KLineData(5L, 120f, 120f, 160f, 110f),
+
+    KLineData(1L, 100f, 120f, 80f, 110f),
+    KLineData(2L, 110f, 130f, 90f, 120f),
+    KLineData(3L, 120f, 140f, 100f, 130f),
+    KLineData(4L, 130f, 150f, 110f, 140f),
+    KLineData(5L, 140f, 160f, 120f, 150f),
+    KLineData(5L, 140f, 160f, 120f, 120f),
+    KLineData(5L, 120f, 120f, 160f, 110f),
+
+    KLineData(1L, 100f, 120f, 80f, 110f),
+    KLineData(2L, 110f, 130f, 90f, 120f),
+    KLineData(3L, 120f, 140f, 100f, 130f),
+    KLineData(4L, 130f, 150f, 110f, 140f),
+    KLineData(5L, 140f, 160f, 120f, 150f),
+    KLineData(5L, 140f, 160f, 120f, 120f),
+    KLineData(5L, 120f, 120f, 160f, 110f)
 
     // Add more sample data as needed
 )
 
-data class KLineData(val open:Float,val close:Float,val high:Float,val low:Float)
+class KLineChartModel(private val data: List<KLineData>) {
+    val maxPrice: Float by lazy { data.maxOf { it.high } }
+    val minPrice: Float by lazy { data.minOf { it.low } }
+
+    var startIndex: Int by mutableIntStateOf(0)
+    var endIndex: Int by mutableIntStateOf( kotlin.math.min(5,data.lastIndex))
+
+    val visibleData: List<KLineData>
+        get() = data.subList(startIndex, endIndex + 1)
+
+
+
+    fun handleDragGesture(dragAmount: Offset) {
+
+        val dragDirection = if (dragAmount.x > 0) {
+            Log.i("zml","向优化")
+            DragDirection.Right
+        } else {
+            Log.i("zml","向作画")
+            DragDirection.Left
+        }
+        val CANDLE_WIDTH = 108f // 设置蜡烛图的宽度
+        val SCREEN_SIZE = 1080 // 设置屏幕的宽度
+
+        val dragDistance = dragAmount.x.absoluteValue
+
+        val visibleDataCount = endIndex - startIndex + 1
+        val maxVisibleDataCount = SCREEN_SIZE / CANDLE_WIDTH
+
+
+        when (dragDirection) {
+            DragDirection.Left -> {
+                if (visibleDataCount < maxVisibleDataCount && endIndex < data.lastIndex) {
+                    endIndex++
+                } else if (startIndex > 0 && dragDistance >= SCREEN_SIZE) {
+                    val moveCount = (dragDistance / CANDLE_WIDTH).toInt()
+                    startIndex -= moveCount
+                    endIndex = startIndex + visibleDataCount - 1
+                }
+            }
+            DragDirection.Right -> {
+                if (visibleDataCount < maxVisibleDataCount && startIndex > 0) {
+                    startIndex--
+                } else if (endIndex < data.lastIndex && dragDistance >= SCREEN_SIZE) {
+                    val moveCount = (dragDistance / CANDLE_WIDTH).toInt()
+                    endIndex += moveCount
+                    startIndex = endIndex - visibleDataCount + 1
+                }
+            }
+        }
+    }
+
+
+    fun updateVisibleData() {
+        // 重新计算可见数据
+        //val visibleData = data.subList(startIndex, endIndex + 1)
+
+    }
+
+    enum class DragDirection {
+        Left, Right
+    }
+}
+
+
+data class KLineData(val timestamp: Long,
+                     val open: Float,
+                     val high: Float,
+                     val low: Float,
+                     val close: Float)
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun KLineChartView(
@@ -97,15 +173,24 @@ fun KLineChartView(
     var translateY by remember { mutableStateOf(0f) }
     val context = LocalContext.current
 
-    val scaleGestureDetector = remember { ScaleGestureDetector(context, ScaleListener()) }
+    val kLineChartModel = remember { KLineChartModel(kLineDataList) }
+
+    //val scaleGestureDetector = remember { ScaleGestureDetector(context, ScaleListener()) }
+
+    val textMeasurer =  rememberTextMeasurer()
 
     Box(
         modifier = modifier
-            .fillMaxSize()
-            .pointerInteropFilter { event ->
-                scaleGestureDetector.onTouchEvent(event)
-                true
+            .pointerInput(Unit){
+                detectDragGestures{change, dragAmount ->
+                    kLineChartModel.handleDragGesture(dragAmount)
+                }
             }
+//            .pointerInteropFilter { event ->
+//
+//                scaleGestureDetector.onTouchEvent(event)
+//                true
+//            }
             .graphicsLayer(
                 scaleX = scaleX,
                 scaleY = scaleY,
@@ -113,12 +198,52 @@ fun KLineChartView(
                 translationY = translateY
             )
     ) {
-        // 绘制K线图
-        Canvas(modifier = Modifier.fillMaxSize()) {
-            // 绘制K线图逻辑
-            drawKLineChart(kLineDataList)
+
+        LaunchedEffect(kLineChartModel.startIndex, kLineChartModel.endIndex) {
+            // 在 startIndex 或 endIndex 发生变化时重新计算可见数据并进行组件刷新
+            kLineChartModel.updateVisibleData()
         }
 
+        // 绘制蜡烛图
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            //
+            KLineChartGrid( maxPrice = kLineChartModel.maxPrice,
+                minPrice = kLineChartModel.minPrice,
+                textMeasurer = textMeasurer)
+            // 绘制K线图逻辑
+            drawKLineChart(kLineChartModel.visibleData)
+        }
+
+    }
+}
+
+
+fun DrawScope.KLineChartGrid(
+    maxPrice: Float,
+    minPrice: Float,
+    textMeasurer:TextMeasurer
+) {
+    val priceRange = maxPrice - minPrice
+    val priceStep = priceRange / 4
+
+    // 绘制网格线
+    for (i in 0..4) {
+        val y = (1 - i / 4f) * size.height
+        drawLine(
+            start = Offset(0f, y),
+            end = Offset(size.width, y),
+            color = Color.LightGray,
+            strokeWidth = 1.dp.toPx()
+        )
+
+        // 绘制价格标签
+        val price = minPrice + i * priceStep
+        drawText(
+            textMeasurer = textMeasurer,
+            text = "$price",
+            style = TextStyle(fontSize = 12.sp),
+            topLeft = Offset(0f, y - 6.dp.toPx())
+        )
     }
 }
 class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener() {
